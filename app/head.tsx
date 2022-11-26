e314189097671e9cfff57c1c3aba1e3081696c33
Anish Pallati <anishp@duck.com>
2022-11-26 16:30:54 -0500

    Initial commit

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+	return (
+		<>
+			<meta charSet="UTF-8" />
+			<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+
+			<link rel="icon" href="/favicon.ico" />
+			<title>Create Next App</title>
+		</>
+	);
+}
---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import { PropsWithChildren } from "react";
+
+export default function RootLayout({ children }: PropsWithChildren) {
+	return (
+		<html lang="en">
+			{/* <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head */}
+			<head />
+			<body>{children}</body>
+		</html>
+	);
+}
