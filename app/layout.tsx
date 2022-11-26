24dfa6c77722c45c9dc54bed694f09d4da97e107
Anish Pallati <anishp@duck.com>
2022-11-26 16:36:12 -0500

    feat: Initialize firebase in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { FirebaseAppProvider } from "reactfire";
 import { PropsWithChildren } from "react";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyD008f0ItuM0teA4W2oMGJPSbDU1n_sV98",
+	authDomain: "idealmd-34853.firebaseapp.com",
+	projectId: "idealmd-34853",
+	storageBucket: "idealmd-34853.appspot.com",
+	messagingSenderId: "986467448186",
+	appId: "1:986467448186:web:94325ad19d1edb59ff12c3",
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
@@ -6,7 +18,11 @@ export default function RootLayout({ children }: PropsWithChildren) {
 			{/* <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head */}
 			<head />
-			<body>{children}</body>
+			<body>
+				<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+					{children}
+				</FirebaseAppProvider>
+			</body>
 		</html>
 	);
 }
